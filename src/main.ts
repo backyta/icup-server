35bae6b294291a0b4fd1b4bb22b51a44cfe31c58
Kevin_BcAng <kevi.baca18@gmail.com>
2025-02-04 12:04:23 -0500

    fix: :bug: test deploy with new domain generic

--- src/main.ts
+++ src/main.ts
@@ -68,7 +68,7 @@ async function bootstrap() {
   const { URL_DOMAIN, STAGE } = process.env;
 
   app.enableCors({
-    origin: STAGE === 'dev' ? 'http://localhost:5173' : URL_DOMAIN,
+    origin: STAGE === 'prod' ? URL_DOMAIN : 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
